799de04fa2455dcb4046f97273c12731d9e94206
TarikHamedovic <thamedovic1@etf.unsa.ba>
2024-06-01 16:30:30 +0200

    Amaranth Support for GateMate
    Created initial files for Amaranth Support on GateMate Chips

---
+++ 1.Blinky/4.amaranth/vendor/__init__.py
@@ -0,0 +1,50 @@
+# The machinery in this module is PEP 562 compliant.
+# See https://peps.python.org/pep-0562/ for details.
+
+
+# Keep this list sorted alphabetically.
+__all__ = [
+    "AlteraPlatform",
+    "GateMatePlatform",
+    "GowinPlatform",
+    "IntelPlatform",
+    "LatticeECP5Platform",
+    "LatticeICE40Platform",
+    "LatticeMachXO2Platform",
+    "LatticeMachXO3LPlatform",
+    "QuicklogicPlatform",
+    "SiliconBluePlatform",
+    "XilinxPlatform",
+]
+
+
+def __dir__():
+    return list({*globals(), *__all__})
+
+
+def __getattr__(name):
+    if name in ("AlteraPlatform", "IntelPlatform"):
+        from ._altera import AlteraPlatform
+        return AlteraPlatform
+    if name == "GateMatePlatform":
+        from ._gatemate import GateMatePlatform
+        return GateMatePlatform
+    if name == "GowinPlatform":
+        from ._gowin import GowinPlatform
+        return GowinPlatform
+    if name == "LatticeECP5Platform":
+        from ._lattice_ecp5 import LatticeECP5Platform
+        return LatticeECP5Platform
+    if name in ("LatticeMachXO2Platform", "LatticeMachXO3LPlatform"):
+        from ._lattice_machxo_2_3l import LatticeMachXO2Or3LPlatform
+        return LatticeMachXO2Or3LPlatform
+    if name == "QuicklogicPlatform":
+        from ._quicklogic import QuicklogicPlatform
+        return QuicklogicPlatform
+    if name in ("SiliconBluePlatform", "LatticeICE40Platform"):
+        from ._siliconblue import SiliconBluePlatform
+        return SiliconBluePlatform
+    if name == "XilinxPlatform":
+        from ._xilinx import XilinxPlatform
+        return XilinxPlatform
+    raise AttributeError(f"module {__name__!r} has no attribute {name!r}")
