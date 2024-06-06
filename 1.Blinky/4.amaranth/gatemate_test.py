b49d48358e6d25d7ca728aa89a334507e8f7e773
TarikHamedovic <thamedovic1@etf.unsa.ba>
2024-06-06 16:50:03 +0200

    Updated Amaranth Blinky example
    Problem is currently in _gatemate.py file in the file_templates section. The file that generates the .ccf constraint file has a problem reading the directions of the attributes specified in gatemate_a1_evb.py.
    So far I have hardcoded the .ccf file in this Blinky Example and it works fine but it needs to be fixed.
    I have asked on the Amaranth Discussion on Github for help.

--- 1.Blinky/4.amaranth/gatemate_test.py
+++ 1.Blinky/4.amaranth/gatemate_test.py
@@ -4,31 +4,32 @@ from amaranth.back import verilog
 
 class Blinky(Elaboratable):
     def __init__(self):
-        self.clk = Signal()
         self.led = Signal()
-
-        self.counter = Signal(10)
+        self.counter = Signal(20)  
 
     def elaborate(self, platform):
-        m = Module()
+        m = Module()    
 
-        m.d.sync += self.counter.eq(self.counter + 1)
-        # Set the LED signal to high
-        m.d.comb += self.led.eq(1)
+        led0 = platform.request('led', 0)
+        
+        with m.If(self.counter < 2**20 - 1):
+            m.d.sync += self.counter.eq(self.counter + 1)
+        with m.Else():
+            m.d.sync += self.counter.eq(0)
+
+        m.d.comb += self.led.eq(self.counter[-1]) 
+        m.d.comb += led0.o.eq(self.led)
+        
         return m
 
 if __name__ == "__main__":
-    
-    m = Module()
     platform = GateMate_A1_EVB()
     top = Blinky()
-    led0 = platform.request('led', 0)
-    m.d.comb += led0.o.eq(top.led)
 
-    ports = [top.clk, top.led, top.counter]
-    verilog_code = verilog.convert(top, ports=ports, name="Blinky", platform=platform)  # Pass platform explicitly
-    with open("Blinky.v", "w") as f:
-        f.write(verilog_code)
+    #ports = [top.led]
+    #verilog_code = verilog.convert(top, ports=ports, name="Blinky", platform=platform)
+    #with open("Blinky.v", "w") as f:
+    #    f.write(verilog_code)
 
-    # Uncomment the line below to build the design
+
     platform.build(top, name="Blinky", do_program=False)
