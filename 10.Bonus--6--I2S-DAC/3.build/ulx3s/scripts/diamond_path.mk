8eadb3dc7f3f51b7cd26eb9667bc381ed6fbdf66
TarikHamedovic <thamedovic1@etf.unsa.ba>
2024-09-23 18:22:12 +0200

    Update

--- 10.Bonus--6--I2S-DAC/3.build/ulx3s/scripts/diamond_path.mk
+++
@@ -1,7 +0,0 @@
-# the path of your diamond installation
-#DIAMOND_BASE ?= /usr/local/diamond
-DIAMOND_BASE ?= /home/user/FPGA/tools/diamond/usr/local/diamond
-
-# it is a directory that looks like this:
-# ls /usr/local/diamond
-# 3.7_x64
