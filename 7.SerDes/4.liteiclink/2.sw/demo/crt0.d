ced33d60ec2bfe432a047d978ba368b4abc90af4
aimamovic6 <aimamovic6@etf.unsa.ba>
2025-07-26 23:45:58 +0200

    Added serdes tool

--- 7.SerDes/4.liteiclink/2.sw/demo/crt0.d
+++
@@ -1,4 +0,0 @@
-crt0.o: \
- /home/hamed/FPGA/Tools/litex-hub/litex/litex/litex/soc/cores/cpu/vexriscv/crt0.S \
- /usr/riscv64-linux-gnu/usr/include/stdc-predef.h
-/usr/riscv64-linux-gnu/usr/include/stdc-predef.h:
